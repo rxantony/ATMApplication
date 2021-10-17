2a0b73f2b47419c797033d98872b408e33f5d02e
rudi antony <rxantony@yahoo.com>
2021-10-17 08:02:13 +0700

    project initialize

---
+++ src/main/java/com/dkatalist/atm/App.java
@@ -0,0 +1,13 @@
+package com.dkatalist.atm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
