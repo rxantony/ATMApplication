a060536dd420cf1f0da6e8ee52e79b870910a41b
Rudi Antony <rudi.antony@mekari.com>
2022-02-19 20:20:04 +0700

    change generic parameter names

--- src/main/java/com/bank/atm/domain/common/ObjectFactory.java
+++ src/main/java/com/bank/atm/domain/common/ObjectFactory.java
@@ -1,5 +1,5 @@
 package com.bank.atm.domain.common;
 
-public interface ObjectFactory<I, O> {
-    O create(I arg);
+public interface ObjectFactory<TInput, TOutput> {
+    TOutput create(TInput arg);
 }
