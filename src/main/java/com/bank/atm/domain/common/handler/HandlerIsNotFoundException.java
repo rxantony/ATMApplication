4fa95d8d8643a986af309cf837829cbd2b0f8f58
Rudi Antony <rudi.antony@mekari.com>
2022-02-20 12:07:47 +0700

    adding handlerName field in HandlerIsNotFoundException

--- src/main/java/com/bank/atm/domain/common/handler/HandlerIsNotFoundException.java
+++ src/main/java/com/bank/atm/domain/common/handler/HandlerIsNotFoundException.java
@@ -1,9 +1,11 @@
 package com.bank.atm.domain.common.handler;
 
 public class HandlerIsNotFoundException extends RuntimeException {
+    public final String handlerName;
 
     public HandlerIsNotFoundException(String handlerName) {
         super(String.format("handler with request %s is not found", handlerName));
+        this.handlerName = handlerName;
     }
 
 }
