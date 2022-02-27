ee89e83483b3ccf211c334fd683d562664bd22ea
Rudi Antony <rudi.antony@mekari.com>
2022-02-27 14:31:25 +0700

    tidy code

--- src/main/java/com/bank/atm/domain/service/user/command/withdraw/WithdrawResult.java
+++ src/main/java/com/bank/atm/domain/service/user/command/withdraw/WithdrawResult.java
@@ -10,7 +10,5 @@ public class WithdrawResult extends TransactionResult {
 
     public WithdrawResult(String accountName, int amount, int balace) {
         super(accountName, amount, balace);
-        setAmount(amount);
-        setBalance(balace);
     }
 }
