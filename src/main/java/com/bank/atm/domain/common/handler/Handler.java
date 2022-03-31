3319489a95381769b5a280862152f8c9e0bb15f2
Rudi Antony <rudi.antony@mekari.com>
2022-03-31 15:07:58 +0700

    back to its pattern

--- src/main/java/com/bank/atm/domain/common/handler/Handler.java
+++ src/main/java/com/bank/atm/domain/common/handler/Handler.java
@@ -1,5 +1,5 @@
 package com.bank.atm.domain.common.handler;
 
-public interface Handler<TRequest, TResult> {
+public interface Handler<TRequest extends Request<TResult>, TResult> {
     TResult execute(TRequest request);
 }
